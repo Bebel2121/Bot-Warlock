3af7a0c894d2a75c113bc5cf9748121748ef49a8
Bebel2121 <bernardofurtado244@gmail.com>
2026-05-24 19:26:14 -0300

    Delete keepalive.js

--- keepalive.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORTA = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('🤖 BOT WARLOCK - ONLINE 24H');
-});
-
-app.listen(PORTA, () => console.log('✅ Servidor de conexão ativo'));
